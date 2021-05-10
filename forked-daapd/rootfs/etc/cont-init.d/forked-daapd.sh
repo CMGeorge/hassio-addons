977aee0b0e01c06064dbe2b92d72a40fead1df47
Chase Lau <chase9@mac.com>
2021-05-10 08:27:35 -0500

    Update to owntone.conf

--- forked-daapd/rootfs/etc/cont-init.d/forked-daapd.sh
+++ forked-daapd/rootfs/etc/cont-init.d/forked-daapd.sh
@@ -9,9 +9,9 @@ if ! bashio::fs.directory_exists '/config/owntone/cache'; then
     mkdir -p /config/owntone/cache
 fi
 
-if ! bashio::fs.file_exists '/config/forked-daapd/forked-daapd.conf'; then
+if ! bashio::fs.file_exists '/config/owntone/owntone.conf'; then
     bashio::log.debug 'Copying default conf file...'
-    cp /usr/local/etc/owntone.conf /config/owntone/forked-daapd.conf
+    cp /usr/local/etc/owntone.conf /config/owntone/owntone.conf
 fi
 
 if ! bashio::fs.directory_exists '/config/owntone/music'; then
