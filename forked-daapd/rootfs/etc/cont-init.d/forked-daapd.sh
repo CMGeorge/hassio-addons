2c20fe5c2cdccc0d20ef2064f0023cb08ba37790
Chase Lau <chase9@mac.com>
2021-05-10 08:21:27 -0500

    Update forked-daapd.sh

--- forked-daapd/rootfs/etc/cont-init.d/forked-daapd.sh
+++ forked-daapd/rootfs/etc/cont-init.d/forked-daapd.sh
@@ -4,19 +4,19 @@
 # Configures the forked-daapd Server
 # ==============================================================================
 
-if ! bashio::fs.directory_exists '/config/forked-daapd/cache'; then
+if ! bashio::fs.directory_exists '/config/owntone/cache'; then
     bashio::log.debug 'Creating cache folder...'
-    mkdir -p /config/forked-daapd/cache
+    mkdir -p /config/owntone/cache
 fi
 
 if ! bashio::fs.file_exists '/config/forked-daapd/forked-daapd.conf'; then
     bashio::log.debug 'Copying default conf file...'
-    cp /usr/local/etc/owntone.conf /config/forked-daapd/forked-daapd.conf
+    cp /usr/local/etc/owntone.conf /config/owntone/forked-daapd.conf
 fi
 
-if ! bashio::fs.directory_exists '/config/forked-daapd/music'; then
+if ! bashio::fs.directory_exists '/config/owntone/music'; then
     bashio::log.debug 'Creating music folder...'
-    mkdir -p /config/forked-daapd/music
+    mkdir -p /config/owntone/music
     bashio::log.debug 'Creating HA fifo file...'
-    mkfifo -m 666 /config/forked-daapd/music/HomeAssistantAnnounce
+    mkfifo -m 666 /config/owntone/music/HomeAssistantAnnounce
 fi
